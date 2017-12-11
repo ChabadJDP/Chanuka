db5fc7613af5f2e5aaaeb2ab4ac2c48932dc0c6a
Mona Lisa <eduardokln@gmail.com>
2017-12-11 13:08:21 -0200

    Atualizacao dos links de pagamento

--- resources/js/var.js
+++ resources/js/var.js
@@ -1,26 +1,26 @@
 var dados = [
   {
     valor: 18,
-    link: 'http://www.uol.com.br',
+    link: 'http://bit.ly/2BcSGM7',
   },
   {
     valor: 36,
-    link: 'http://www.terra.com.br',
+    link: 'http://bit.ly/2BIAcAq',
   },
   {
     valor: 54,
-    link: 'http://www.globo.com',
+    link: 'http://bit.ly/2BdvxJx',
   },
   {
     valor: 72,
-    link: 'http://www.globo.com',
+    link: 'http://bit.ly/2Ba75J0',
   },
   {
     valor: 90,
-    link: 'http://www.globo.com',
+    link: 'http://bit.ly/2BdwVMf',
   },
   {
     valor: 108,
-    link: 'http://www.globo.com',
+    link: 'http://bit.ly/2Bc3jyY',
   },
 ];
